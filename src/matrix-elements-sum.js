4d56517a4d599c5fa77a42d351e338dcc74753a1
Dias1c <dias.kappasov@gmail.com>
2024-12-22 19:56:23 +0500

    feat: solved matrix-elements-sum.js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  if (!Array.isArray(matrix) || matrix.length == 0 || matrix[0].length == 0) {
+    return 0;
+  }
+
+  const getSize = (x, y) => {
+    if (
+      x < 0 ||
+      y < 0 ||
+      y >= matrix.length ||
+      x >= matrix[0].length ||
+      matrix[y][x] <= 0
+    ) {
+      return 0;
+    }
+
+    let value = matrix[y][x];
+    matrix[y][x] = 0;
+
+    return (
+      value +
+      getSize(x + 1, y) +
+      getSize(x - 1, y) +
+      getSize(x, y + 1) +
+      getSize(x, y - 1)
+    );
+  };
+
+  let result = 0;
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[0].length; x++) {
+      if (matrix[y][x] == 0) {
+        continue;
+      }
+
+      const sum = getSize(x, y);
+      result = Math.max(sum, result);
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
